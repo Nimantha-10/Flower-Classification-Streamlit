c61aaf295612d0dbc481362345e7df5ecb3b0854
Nimantha-10 <nimantha1020@gmail.com>
2024-08-03 22:35:21 +0530

    app.py

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import numpy as np
 import requests
 from io import BytesIO
 
-# URL to the model file stored on a cloud storage
+# URL to the model file stored on Google Drive
 MODEL_URL = "https://drive.google.com/uc?export=download&id=1--BsZo7orLcfLT7YP82w-j0qX8Y7GtS5"
 
 @st.cache(allow_output_mutation=True)
 def load_model():
     try:
         response = requests.get(MODEL_URL)
-        response.raise_for_status()  # Check if the request was successful
+        response.raise_for_status()  # Check for HTTP request errors
         model = tf.keras.models.load_model(BytesIO(response.content))
+        st.success("Model loaded successfully!")
         return model
     except Exception as e:
         st.error(f"Error loading model: {e}")
@@ -24,16 +25,22 @@ model = load_model()
 categories = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
 def predict_flower(image, model, categories):
-    image = Image.open(image)
-    image = image.resize((150, 150))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-    predictions = model.predict(image)
-    pred_class = np.argmax(predictions)
-    confidence = np.max(predictions)
-    if confidence < 0.5:
-        return "Not in system"
-    return categories[pred_class]
+    if model is None:
+        return "Model is not loaded"
+    
+    try:
+        image = Image.open(image)
+        image = image.resize((150, 150))
+        image = np.array(image) / 255.0
+        image = np.expand_dims(image, axis=0)
+        predictions = model.predict(image)
+        pred_class = np.argmax(predictions)
+        confidence = np.max(predictions)
+        if confidence < 0.5:
+            return "Not in system"
+        return categories[pred_class]
+    except Exception as e:
+        return f"Error in prediction: {e}"
 
 st.title("Flower Classification")
 st.header("Upload a picture of a flower to identify it.")
@@ -43,8 +50,5 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
     st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
     if st.button("Predict"):
-        if model:
-            flower_name = predict_flower(uploaded_file, model, categories)
-            st.write(f"The predicted flower is: {flower_name}")
-        else:
-            st.write("Model not loaded successfully.")
+        flower_name = predict_flower(uploaded_file, model, categories)
+        st.write(f"The predicted flower is: {flower_name}")
